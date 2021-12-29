184113320b2d955b9420274ca43b5aa4100ae810
Noor Ncho <nrandin@gmail.com>
2021-12-29 10:04:09 +0000

    removed authentication variables

--- src/pages/landing-page/landing-page.jsx
+++ src/pages/landing-page/landing-page.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import * as SpotifyAuth from "../../assets/js/spotifyAuth";
+//import * as SpotifyAuth from "../../assets/js/spotifyAuth";
 
 const LandingPage = () => {
+    const {REACT_APP_CLIENT_ID, REACT_APP_AUTH_ENDPOINT, REACT_APP_REDIRECT_URL} = process.env;
+    
+    const scope = [
+        'user-top-read',
+        'user-follow-read',
+        'user-read-recently-played',
+        'user-read-currently-playing',
+        'streaming'
+    ];
+
+    const getAuthentication = () =>{
+        let url = REACT_APP_AUTH_ENDPOINT;
+        url += '?client_id=' + REACT_APP_CLIENT_ID;
+        url += '&redirect_uri=' + REACT_APP_REDIRECT_URL;
+        url += '&scope=' + scope.join("%20");
+        url += "&response_type=token";
+        url += "&show_dialog=true";
+    
+        window.location.href = url;
+    }
 
     return (
         <div>
-            <button type="submit" onClick={SpotifyAuth.getAuthentication}>Login to Spotify</button>
+            <button type="submit" onClick={/*SpotifyAuth.*/getAuthentication}>Login to Spotify</button>
         </div>
     )
 }
